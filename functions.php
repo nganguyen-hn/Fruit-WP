74d943fd0d2c0a5e70bd1e3ff2a8c20e8b19ff42
Nga <nganguyen22790@gmail.com>
2020-10-07 15:01:25 +0700

    file funtion+ index test

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'after_setup_theme', 'setup_theme' );
+function setup_theme(){
+	add_theme_support('post-thumbnails');
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+get_header(); 
+?>
+<main>
+	index
+</main>
+ <?php get_footer(); ?>

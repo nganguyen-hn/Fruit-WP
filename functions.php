9d8dce9b580c997e319ad8cdfa13101f42906d6e
DESKTOP-8G65177\Boostify <nganguyen22790@gmail.com>
2020-10-23 16:22:58 +0700

    include -> require

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
-include ('include/widget_search.php');
-include ('include/widget_new_post.php');
-include ('include/widget_list_tag.php');
-include ('include/widget_category_product.php');
-include('include/bs4navwalker.php');
+require get_template_directory() . '/include/widget_search.php';
+require get_template_directory() . '/include/widget_new_post.php';
+require get_template_directory() . '/include/widget_list_tag.php';
+require get_template_directory() . '/include/widget_category_product.php';
+require get_template_directory() . '/include/bs4navwalker.php';
 add_action( 'after_fruit_setup', 'fruit_setup' );
 add_filter( 'widget_text', 'do_shortcode' );
 function fruit_setup(){
@@ -105,15 +105,15 @@ function wpbeginner_numeric_posts_navfruit() {
     if( is_singular() )
         return;
     global $wp_query;
-    // Stop execution if there's only 1 page 
+    // Stop execution if there's only 1 page
     if( $wp_query->max_num_pages <= 1 )
         return;
     $paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
     $max   = intval( $wp_query->max_num_pages );
-    // Add current page to the array 
+    // Add current page to the array
     if ( $paged >= 1 )
         $links[] = $paged;
-    // Add the pages around the current page to the array 
+    // Add the pages around the current page to the array
     if ( $paged >= 3 ) {
         $links[] = $paged - 1;
         $links[] = $paged - 2;
@@ -123,30 +123,30 @@ function wpbeginner_numeric_posts_navfruit() {
         $links[] = $paged + 1;
     }
     echo '<div class="pagination-nav text-center"><ul class="p-0 mb-0 list-unstyled">' . "\n";
-    // Previous Post Link 
+    // Previous Post Link
     if ( get_previous_posts_link() )
         printf( '<li class="disabled">%s</li>' . "\n", get_previous_posts_link('<i class="fa fa-angle-left"></i>') );
-    // Link to first page, plus ellipses if necessary 
+    // Link to first page, plus ellipses if necessary
     if ( ! in_array( 1, $links ) ) {
         $class = 1 == $paged ? ' class="active"' : '';
         printf( '<li%s><a href="%s"  class="">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
         if ( ! in_array( 2, $links ) )
             echo '<li>…</li>';
     }
-    // Link to current page, plus 2 pages in either direction if necessary 
+    // Link to current page, plus 2 pages in either direction if necessary
     sort( $links );
     foreach ( (array) $links as $link ) {
         $class = $paged == $link ? ' class="active"' : '';
         printf( '<li%s><a href="%s"  class="">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
     }
-    // Link to last page, plus ellipses if necessary 
+    // Link to last page, plus ellipses if necessary
     if ( ! in_array( $max, $links ) ) {
         if ( ! in_array( $max - 1, $links ) )
             echo '<li>…</li>' . "\n";
         $class = $paged == $max ? ' class="active"' : '';
         printf( '<li%s><a href="%s"  class="">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
     }
-    // Next Post Link 
+    // Next Post Link
     if ( get_next_posts_link() )
         printf( '<li class="">%s</li>' . "\n", get_next_posts_link('<i class="fa fa-angle-right"></i>') );
     echo ' </ul></div>' . "\n";
@@ -206,9 +206,7 @@ if ( ! function_exists( 'fruit_comment_list' ) ) {
                                     ?>
                                 <?php edit_comment_link( esc_html__( 'Edit' ), ' ', '' ); ?>
                             </div>
-                        </div>                                         
-                        
-                        
+                        </div>
                     </div>
                 </div>
                 <?php
@@ -217,6 +215,8 @@ if ( ! function_exists( 'fruit_comment_list' ) ) {
     }
 }
 
+
+require get_template_directory() . '/custom-widgets/my-widgets.php';
 
 
 function fruit_date_format() {
